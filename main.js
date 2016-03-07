8dc774cc735cc48242938e3a6706cda89d0c59f4
Vito Huang <vito.vvsh.huang@gmail.com>
2016-03-07 22:03:24 +0000

    using web worker to do setInterval/count, because on chrome the timer will a bit inactive when the tab is not focus

--- main.js
+++ main.js
@@ -1,100 +1,8 @@
-function Pomodoro(requestTime) {
-
-    // Get the audio
-    this.audio = new Audio('http://www.createjs.com/demos/_assets/audio/Game-Shot.ogg');
-		this.pomodoroTime = requestTime;
-		this.allowPopup = false;
-
-		this.initTimer(this.pomodoroTime);
-
-		// Set the initial state
-		this.state = {
-			toggleTimerText: 'Start',
-			counting: false,
-			displayTime: this.getDisplayTime(),
-		};
-
-		// Request popup permission
-		this.requestPopupPermission();
-
-		this.displayEl = document.getElementById('display-time');
-		this.toggleEl = document.getElementById('toggle-timer');
-		this.clearEl = document.getElementById('clear-timer');
-
-		// Add events
-		this.addEvents();
-
-		// Set up the initial result for the current state 
-		this.changeStateEffects();
-};
-
-Pomodoro.prototype = {
-	setState: function(states) {
-		for (prop in states) {
-			this.state[prop] = states[prop];
-		}	
-
-		this.changeStateEffects();
-	},
-
-	changeStateEffects: function() {
-		if (this.state.counting) {
-			this.toggleEl.className = 'toggle-timer pause';
-			this.toggleEl.textContent = 'Pause';
-		} else {
-			this.toggleEl.className = 'toggle-timer start';
-			this.toggleEl.textContent = 'Start';
-		}
-
-		if (this.state.toggleTimerText) {
-			this.toggleEl.textContent = this.state.toggleTimerText;
-		}
-
-		if (this.displayEl) {
-			this.displayEl.textContent = this.getDisplayTime();
-		}
-	},
-
-	initTimer: function(minutes) {
-		this.momentDuration = moment.duration(this.pomodoroTime, 'minutes');
-	},
-
-	addEvents: function() {
-
-		this.toggleEl.addEventListener('click', this.toggleTimer.bind(this));
-		this.clearEl.addEventListener('click', this.clearTimer.bind(this));
-
-		document.addEventListener("visibilitychange", this.handleVisibilityChange.bind(this), false);
-	},
-
-	handleVisibilityChange: function(event) {
-		if (document.hidden) {
-			// the tab is not not in focus
-			console.log("tab is not focus");
-		} else {
-			console.log("tab is focus");
-		}	
-	},
-
-	// Get display time
-	getDisplayTime: function() {
-		var seconds = this.momentDuration.seconds();
-		var minutes = this.momentDuration.minutes();
-
-		// Pad 0
-		if (seconds < 10) {
-			seconds = '0' + seconds;
-		}
-
-		if (minutes < 10) {
-			minutes = '0' + minutes;
-		}
-
-		return minutes + ':' + seconds;
-	},
+var allowPopup = false;
+var audio = new Audio('http://www.createjs.com/demos/_assets/audio/Game-Shot.ogg');
 
 	// Show notification
-	showNotification: function(title, body, icon) {
+function showNotification(title, body, icon) {
 		// Default title
 		var t = title || "Hi";
 
@@ -111,89 +19,82 @@ Pomodoro.prototype = {
 		n.onclick = function() {
 			n.close();
 		}
-	},
+}
 
 	// Request for notification permission
-	requestPopupPermission: function() {
+function requestPopupPermission() {
 		Notification.requestPermission(function(result) {
 			// Check permission
 			if (result === "denied") {
 				return;
 			} else if (result === 'default' || result === "granted") {
-				this.allowPopup = true;
+				allowPopup = true;
 			}
-		}.bind(this));
-	},
-
-	toggleTimer: function(event) {
-		// See if this counting or not
-		if (this.state.counting) {
-			this.setState({counting: false});
-			this.setState({toggleTimerText: 'Start'});
-
-			// Pause timer
-			this.pauseTimer();
-		} else {
-			this.setState({counting: true});
-			this.setState({toggleTimerText: 'Pause'});
-
-			// Start the timer
-			this.startTimer();
-		}
-	},
-
-	startTimer: function() {
-    this.interval = setInterval(() => {
-				this.tick();
-		}, 1000);
-
-		// Change the button state text
-		this.setState({counting: true});
-	},
-
-	pauseTimer: function() {
-    clearInterval(this.interval);
-	},
-
-	clearTimer: function() {
-		// Pause the timer
-		if (this.state.counting) {
-			this.pauseTimer();
-		}
-
-		// Clear the timer and state
-		this.initTimer(this.pomodoroTime);
-		this.setState({
-			counting: false,
-			toggleTimerText: 'Start',
-			displayTime: this.getDisplayTime(),
 		});
-	},
-
-	tick: function() {
-		//console.log("ticking");
-		//console.log(this.momentDuration.seconds());
-		if (this.momentDuration.valueOf() == 0) {
-			// Check if can show popup
-			if (this.allowPopup) {
-				this.showNotification('Pomodoro', 'Its time to take a rest');
-			}
-
-      // Play a sound
-      this.audio.play();
-
-			// Clear timer
-			this.clearTimer();
-		} else {
-			// Subtract a second from it
-			this.momentDuration.subtract(1, 's');
-
-			// Set the state
-			this.setState({displayTime: this.getDisplayTime() });
-		}
-  },
-};
+}
 
 window.onload = function() {
-	var p = new Pomodoro(12);
-};
+
+		// Request popup permission
+		requestPopupPermission();
+
+		var displayEl = document.getElementById('display-time');
+		var toggleEl = document.getElementById('toggle-timer');
+		var clearEl = document.getElementById('clear-timer');
+
+		// Web worker to count time, because when the tab is inactive, setInterval run slower
+		var myWorker = new Worker("pomodoro.js");
+		myWorker.onmessage = function(event) {
+			console.log("received message from worker");
+			console.log(event.data);
+			changeStateEffects(event.data);
+		}
+
+		myWorker.onerror = function(event) {
+			console.log("there is an error");
+			console.log(event);
+		}
+		
+		// Initialise an pomodor
+		var requestTime = 1;
+		window.onhashchange = function(event) {
+			if (window.location.hash.length > 1) {
+				requestTime = parseInt(window.location.hash.slice(1));
+				myWorker.postMessage(['init', requestTime]);
+				myWorker.postMessage(['toggle']);
+			}
+		}
+		myWorker.postMessage(['init', requestTime]);
+
+		// Add events to toggle and clear buttons
+		toggleEl.addEventListener('click', function(event) {
+			myWorker.postMessage(['toggle']);
+		});
+		clearEl.addEventListener('click', function(event) {
+			myWorker.postMessage(['clear']);
+		});
+
+		var changeStateEffects = function(state) {
+			if (state.counting) {
+				toggleEl.className = 'toggle-timer pause';
+				toggleEl.textContent = 'Pause';
+			} else {
+				toggleEl.className = 'toggle-timer start';
+				toggleEl.textContent = 'Start';
+			}
+
+			if (state.toggleTimerText) {
+				toggleEl.textContent = state.toggleTimerText;
+			}
+
+			if (displayEl) {
+				displayEl.textContent = state.displayTime;
+			}
+
+			// This is finish
+			if (state.finish) {
+				showNotification('Pomodoro', 'Its time to take a rest');
+				audio.play();
+			}
+		}
+}
---
+++ pomodoro.js
@@ -0,0 +1,137 @@
+// Use moment js
+importScripts("moment.js");
+
+function Pomodoro(requestTime) {
+		// Set the initial state
+		this.state = {
+			counting: false,
+			toggleTimerText: 'Start',
+			displayTime: '',
+			finish: false,
+		};
+
+    // Get the audio
+		this.pomodoroTime = requestTime;
+
+		this.initTimer(this.pomodoroTime);
+};
+
+Pomodoro.prototype = {
+	setState: function(states) {
+		for (prop in states) {
+			this.state[prop] = states[prop];
+		}	
+
+		// Post the resutl back into main thread
+		postMessage(this.state);
+	},
+
+
+	initTimer: function(minutes) {
+		this.momentDuration = moment.duration(this.pomodoroTime, 'minutes');
+
+		// Set the display time
+		this.setState({displayTime: this.getDisplayTime()});
+	},
+
+
+	// Get display time
+	getDisplayTime: function() {
+		var seconds = this.momentDuration.seconds();
+		var minutes = this.momentDuration.minutes();
+
+		// Pad 0
+		if (seconds < 10) {
+			seconds = '0' + seconds;
+		}
+
+		if (minutes < 10) {
+			minutes = '0' + minutes;
+		}
+
+		return minutes + ':' + seconds;
+	},
+
+	toggleTimer: function() {
+		// See if this counting or not
+		if (this.state.counting) {
+			this.setState({counting: false});
+			this.setState({toggleTimerText: 'Start'});
+
+			// Pause timer
+			this.pauseTimer();
+		} else {
+			this.setState({counting: true});
+			this.setState({toggleTimerText: 'Pause'});
+
+			// Start the timer
+			this.startTimer();
+		}
+	},
+
+	startTimer: function() {
+    this.interval = setInterval(() => {
+				this.tick();
+		}, 1000);
+
+		// Change the button state text
+		this.setState({counting: true});
+	},
+
+	pauseTimer: function() {
+    clearInterval(this.interval);
+	},
+
+	clearTimer: function() {
+		// Pause the timer
+		if (this.state.counting) {
+			this.pauseTimer();
+		}
+
+		// Clear the timer and state
+		this.setState({
+			counting: false,
+			toggleTimerText: 'Start',
+			finish: false,
+		});
+
+		// Initialise a new timer
+		this.initTimer(this.pomodoroTime);
+	},
+
+	tick: function() {
+		if (this.momentDuration.valueOf() == 0) {
+			// Set the state to finish
+			this.setState({finish: true});
+
+			// Clear timer
+			this.clearTimer();
+		} else {
+			// Subtract a second from it
+			this.momentDuration.subtract(1, 's');
+
+			// Set the state
+			this.setState({displayTime: this.getDisplayTime() });
+		}
+  },
+};
+
+var p = null;
+onmessage = function(event) {
+	console.log("this is web worker - received message");
+	console.log(event.data);
+
+	var command = event.data[0];
+	var arg = event.data.slice(1);
+	if (command == 'init') {
+		// Clear the timer if its already there
+		if (p) {
+			p.clearTimer();
+		}
+		p = new Pomodoro(arg[0]);
+	} else if (command == 'toggle') {
+		p.toggleTimer();
+	} else if  (command == 'clear') {
+		p.clearTimer();
+	}
+}
